23f7a66a1af3bdb8d2052944ef519a362a9c3227
vishalMakthala <114413722+vishalMakthala@users.noreply.github.com>
2023-12-07 03:59:28 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+// let score = "vishal"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log( valueNumber);
+// "33"  => 33
+// "33bc" => NAN
+// true => false =>0
+let islggedIn ="vishal"
+let booleanIsloggedIn = Boolean(islggedIn)
+//console.log(booleanIsloggedIn)
+
+//1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
+/// ************************************* OPerations *********************************** /////
+
+let value =  3
+let negative = -value
+console.log(negative)
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+'2');
+console.log("1"+2);
+
