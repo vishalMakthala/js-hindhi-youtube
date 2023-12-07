058c8d4a5aa87389b38c9114c2de364e28af49a4
vishalMakthala <114413722+vishalMakthala@users.noreply.github.com>
2023-12-07 12:59:21 +0000

    string basic

---
+++ 01_basics/05_srting.js
@@ -0,0 +1,7 @@
+const name = "hitesh"
+const repoCount = 50
+console.log(name + repoCount +" VAlue");
+console.log(`Hello my ${name} and my repo count is ${repoCount}`);
+//this is the morden way of syntax this called sting interpolatin
+// the advantages of writing like is this can be modified on the go
+// like make name upper case by adding syntax(.toUppercase)  
