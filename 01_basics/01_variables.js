54b0b51bf1c9fa87502649f70f7020b6c6b18792
vishalMakthala <114413722+vishalMakthala@users.noreply.github.com>
2023-12-07 03:58:48 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "visha@gmai.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+
+//accountId = 22559966
+accountEmail = "hc@hc.com"
+accountPassword = " 2121212100"
+accountCity = " Bengaluru"
+
+console.log(accountId);
+/*
+prefer not to use var
+beacause of issue in block scope and functional scppe
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
