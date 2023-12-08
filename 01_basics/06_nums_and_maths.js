e384f3a83c9fc31790cf6dccfb448735a371d805
vishalMakthala <114413722+vishalMakthala@users.noreply.github.com>
2023-12-08 02:53:43 +0000

    maths and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toString());
+
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+////console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+// maths .random gives values between 0 & 1 
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1) + min))
