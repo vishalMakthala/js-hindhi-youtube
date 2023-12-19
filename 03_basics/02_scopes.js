b3431411d3fbf4afb79f723b048b7a9cd9195f76
vishalMakthala <114413722+vishalMakthala@users.noreply.github.com>
2023-12-19 13:05:50 +0000

    02_Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,31 @@
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(webkitURL);
+    two()
+}
+one()
+
+if (true){
+    const username="histesh"
+    if(username === "histesh"){
+        const website = "youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+
+}
+// console.log(username);
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+addTow(5)
+const addTow = function(num){
+    return num + 2
+}
