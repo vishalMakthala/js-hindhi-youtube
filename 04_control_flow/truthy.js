3f7fb35400d1d55d2710a8f42ec85cbb480d0385
vishalMakthala <114413722+vishalMakthala@users.noreply.github.com>
2023-12-20 13:51:26 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,22 @@
+const userEmail = "vishalmakthala"
+
+if (userEmail){
+    // console.log("Got email");
+}else{
+    // console.log("Dont have user email");
+    
+}
+// falsy values
+// false, 0 ,-0, BigInt on , null, undefiend NaN
+
+// truthy value
+//"0","false"," ",[],{}, function
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+// console.log(val1);
+
+//  Ternary Operator 
